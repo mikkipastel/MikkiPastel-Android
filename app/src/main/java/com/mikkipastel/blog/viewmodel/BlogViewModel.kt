abd53857603be65c2f9f37f0bc3c46d8e9d69b85
Monthira Chayabanjonglerd <monthira@ookbee.com>
2021-01-01 21:40:48 +0700

    refactor(app): refactor for init LiveData variable

--- app/src/main/java/com/mikkipastel/blog/viewmodel/BlogViewModel.kt
+++ app/src/main/java/com/mikkipastel/blog/viewmodel/BlogViewModel.kt
@@ -10,23 +10,13 @@ import kotlinx.coroutines.*
 
 class BlogViewModel(private val blogRepository: BlogRepository) : ViewModel() {
 
-    private val _allBlogPost = MutableLiveData<MutableList<PostBlog>>()
-    val allBlogPost = _allBlogPost
+    val allBlogPost = MutableLiveData<MutableList<PostBlog>>()
+    val canLazyLoading = MutableLiveData<Boolean>()
+    val blogPage = MutableLiveData<Int>()
+    val getBlogError = MutableLiveData<Unit>()
 
-    private val _canLazyLoading = MutableLiveData<Boolean>()
-    val canLazyLoading = _canLazyLoading
-
-    private val _blogPage = MutableLiveData<Int>()
-    val blogPage = _blogPage
-
-    private val _getBlogError = MutableLiveData<Unit>()
-    val getBlogError = _getBlogError
-
-    private val _allBlogTag = MutableLiveData<MutableList<TagBlog>>()
-    val allBlogTag = _allBlogTag
-
-    private val _getTagError = MutableLiveData<Unit>()
-    val getTagError = _getTagError
+    val allBlogTag = MutableLiveData<MutableList<TagBlog>>()
+    val getTagError = MutableLiveData<Unit>()
 
     val localBlogTagList = MutableLiveData<MutableList<TagBlog>>()
     val localBlogContentList = MutableLiveData<MutableList<PostBlog>>()
@@ -34,13 +24,13 @@ class BlogViewModel(private val blogRepository: BlogRepository) : ViewModel() {
     fun getBlogPost(page: Int, hashtag: String?) {
         val tag = if (hashtag == null) "" else "tag:$hashtag"
         val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-            _getBlogError.value = Unit
+            getBlogError.value = Unit
         }
         viewModelScope.launch(exceptionHandler) {
             val response = blogRepository.getBlogPost(page, tag)
-            _allBlogPost.value = response.posts
-            _canLazyLoading.value = response.meta?.pagination?.next != null
-            _blogPage.value = response.meta?.pagination?.page
+            allBlogPost.value = response.posts
+            canLazyLoading.value = response.meta?.pagination?.next != null
+            blogPage.value = response.meta?.pagination?.page
             withContext(Dispatchers.IO) {
                 if (hashtag == null && response.meta?.pagination?.page == 1)
                     blogRepository.insertAllBlogToRoom(response.posts!!)
@@ -56,11 +46,11 @@ class BlogViewModel(private val blogRepository: BlogRepository) : ViewModel() {
 
     fun getBlogTag() {
         val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-            _getTagError.value = Unit
+            getTagError.value = Unit
         }
         viewModelScope.launch(exceptionHandler) {
             val response = blogRepository.getBlogTag()
-            _allBlogTag.value = response.tags
+            allBlogTag.value = response.tags
             withContext(Dispatchers.IO) {
                 blogRepository.insertAllTagToRoom(response.tags)
             }
