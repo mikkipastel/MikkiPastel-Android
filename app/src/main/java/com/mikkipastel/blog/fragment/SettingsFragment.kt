db90d7111668e226f516e5957f9abfbb27806000
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2021-09-15 20:30:31 +0700

    feature(app): add open source licenses

--- app/src/main/java/com/mikkipastel/blog/fragment/SettingsFragment.kt
+++ app/src/main/java/com/mikkipastel/blog/fragment/SettingsFragment.kt
@@ -1,11 +1,24 @@
 package com.mikkipastel.blog.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.mikkipastel.blog.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_setting, rootKey)
+
+        val openSourcePreference = findPreference<Preference>("open_source_menu")
+        openSourcePreference?.setOnPreferenceClickListener {
+            startActivity(
+                Intent(
+                    requireContext(),
+                    OssLicensesMenuActivity::class.java)
+            )
+            true
+        }
     }
 }
